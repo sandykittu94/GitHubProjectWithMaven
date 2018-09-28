61150da5e4699860e820a2b5ef32793ed5053743
Santosh Kumar <you@example.com>
2018-09-28 13:26:21 +0530

    First commit

---
+++ src/test/java/com/citibank/login/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.citibank.login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+
+	@Test
+	public void login() {
+
+		System.out.println("Hello one");
+		System.out.println("Hello one");
+		System.out.println("Hello one");
+		System.out.println("Hello one");
+		System.out.println("Hello one");
+
+	}
+
+
+
+}
